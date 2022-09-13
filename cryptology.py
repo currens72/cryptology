61c71ff9faae740f1518dd387cf4b426abdfa266
Thomas <trglass@iastate.edu>
2022-09-13 12:40:49 -0500

    Added a feature to check for 100 most common words. This will be used in later features to automatically decrypt.

--- cryptology.py
+++ cryptology.py
@@ -3,6 +3,16 @@ import string
 from typing import Counter
 from collections import Counter
 
+#100 most common words according to wikipedia.org
+commonWord = ["THE", "BE", "TO", "OF", "AND", "IN", "THAT", "HAVE", "IT", "FOR", "NOT", "ON", "WITH", "HE", "AS", "YOU", "DO", "AT", "THIS", "BUT", "HIS", "BY", "FROM", "THEY", "WE", "SAY", "HER", "SHE", "OR", "AN", "WILL", "MY", "ONE", "ALL", "WOULD", "THERE", "THEIR", "WHAT", "SO", "UP", "OUT", "IF", "ABOUT", "WHO", "GET", "WHICH", "GO", "ME", "WHEN", "MAKE", "CAN", "LIKE", "TIME", "NO", "JUST", "HIM", "KNOW", "TAKE", "PEOPLE", "INTO", "YEAR", "YOUR", "GOOD", "SOME", "COULD", "THEM", "THEM", "SEE", "OTHER", "THAN", "THEN", "NOW", "LOOK", "ONLY", "COME", "ITS", "OVER", "THINK", "ALSO", "BACK", "AFTER", "USE", "TWO", "HOW", "OUR", "WORK", "FIRST", "WELL", "WAY", "EVEN", "NEW", "WANT", "BECAUSE", "ANY", "THESE", "GIVE", "DAY", "MOST", "US"]
+
+#function to check if 100 most common words are contained in decryption
+def contains(string):
+    if any(ele in string for ele in commonWord):
+        return True
+    else:
+        return False
+
 #function to manually shift by to decrypt text
 def decrypt():
     text = input("Enter shift-by text here: ")
@@ -55,3 +65,6 @@ def trigram():
     print(trigramCount)
 
 alphabet = string.ascii_uppercase
+
+testing = input("Enter text here: ")
+print(contains(testing))
